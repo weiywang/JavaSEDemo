6db5a58882efc88cb67a53dfbed813293ef8d8cf
wangweiya <sa517370@mail.ustc.edu.cn>
2019-08-17 10:42:36 +0800

    BubbleSort 1.0

---
+++ src/PrepareForPuFa/BubbleSort.java
@@ -0,0 +1,28 @@
+package PrepareForPuFa;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int[] array){
+        if(array.length == 0){
+            return null;
+        }
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length-1-i; j++){
+                if(array[j+1] < array[j]){
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] Args){
+        int[] array = {1,3,2,5,8,4,6};
+        int[] res = bubbleSort(array);
+        for(int i = 0; i < res.length; i++){
+            System.out.println(res[i]);
+        }
+    }
+}
