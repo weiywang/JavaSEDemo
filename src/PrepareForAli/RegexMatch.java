0d563244db2aa759ea34e99d4bac106ff81776c0
wangweiya <sa517370@mail.ustc.edu.cn>
2019-08-18 21:51:27 +0800

    RegexMatch 3.0

--- src/PrepareForAli/RegexMatch.java
+++ src/PrepareForAli/RegexMatch.java
@@ -13,7 +13,8 @@ public class RegexMatch {
             for(int j = 1; j <= plen; j++){
                 if(p.charAt(j-1) == '*'){
                     //dp[i-1][j]? s.charAt(i-1) == p.charAt(j-2)?
-                    //在本轮计算中，考察的是j-1，因此如果*什么也没代替，就自然考虑j-2
+                    //在本轮计算中，考察的是j-1，因此如果*什么也没代替，就自然考虑j-2，dp[i][j-2]
+                    //如果*替代了至少一个前面的字符，则一定有s[i-1]=p[j-2]或者p[j-2]=0,这时有dp[i][j]=dp[i-1][j]
                     dp[i][j] = dp[i][j-2] || (i>0 && dp[i-1][j] && (s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.'));
                 }else{
                     dp[i][j] = i > 0 && dp[i-1][j-1] && (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.');
