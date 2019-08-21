dad229d3aa423a8676b7493e888bd4a79d7b95b9
wangweiya <sa517370@mail.ustc.edu.cn>
2019-08-21 19:58:39 +0800

    Jump 2.0

--- src/PrepareForAli/GreedyAlgorithm/Jump.java
+++ src/PrepareForAli/GreedyAlgorithm/Jump.java
@@ -6,7 +6,7 @@ public class Jump {
         int end = 0;
         int maxPosition = 0;
 
-        //nums.length - 1?
+        //nums.length - 1? nums.length - 1如何处理的？当 i = nums.length - 1时跳出了循环
         for(int i = 0; i < nums.length - 1; i++){
             maxPosition = Math.max(maxPosition, nums[i] + i);
             if(i == end){
